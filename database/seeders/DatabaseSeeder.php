a3e1d7290c3a11fc6571c40b46d83a35c66ba612
eryherdinata <ery.herdinata@shipper.id>
2025-07-17 16:56:06 +0700

    update: composer + seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        /* User::factory()->create([
             'name' => 'Test User',
             'username' => 'test_user',
             'email' => 'test@example.com',
-        ]);
+        ]); */
     }
 }
